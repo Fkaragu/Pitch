641d46c57a50a5cb9941fe61c2ca51389ff2a655
Francis Karagu <fkaragu@gmail.com>
2019-02-10 18:36:06 +0300

    Added profile section

--- app/auth/views.py
+++ app/auth/views.py
@@ -35,11 +35,11 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        # mail_message('Welcome to Watchlist', 'email/welcome_user', user.email, user=user)
+        # mail_message('Welcome to one minute pitch', 'email/welcome_user', user.email, user=user)
         send_email(subject="Registration", sender=os.environ.get('MAIL_USERNAME'),recepients=[user.email],text_body='Test Email',html_body=render_template('500.html'))
 
         return redirect(url_for('auth.login'))
 
-    title = "New Account | One Minute Pitch "
+    title = "New Account"
 
     return render_template('auth/register.html',registration_form = form, title = title)
