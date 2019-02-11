cd07812e98fa00c8a00cbb8fd628f06d8a503f90
Francis Karagu <fkaragu@gmail.com>
2019-02-11 23:10:54 +0300

    Added class for Pickupline

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,16 +8,16 @@ class ReviewForm(FlaskForm):
     review = TextAreaField('Movie review', validators=[Required()])
     submit = SubmitField('Submit')
 
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write something about yourself',validators=[Required()])
-    submit = SubmitField('Submit')
-
 class CommentForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[Required()])
     submit = SubmitField('Post Comment')
 
-class PitchForm(FlaskForm):
-    pitch = TextAreaField('Your Pitch', validators=[Required()])
-    # my_category = StringField('Category', validators=[Required()])
-    my_category = SelectField('Category', choices=[('Interview-Pitch','Interview Pitch'),('Product-Pitch','Product Pitch'),('Promotion-Pitch','Promotion Pitch'),('Business','Business'),('Academic','Academic'),('Political','Political'),('Technology','Technology'),('Health','Health')],validators=[Required()])
-    submit = SubmitField('Pitch!')
+class PitchFormI(FlaskForm):
+    pitch = TextAreaField('Pitch Comment', validators=[Required()])
+    my_category = SelectField('Category', choices=[('Interview','Interview')])
+    submit = SubmitField('Submit')
+
+class PitchFormL(FlaskForm):
+    pitch = TextAreaField('Pitch Comment', validators=[Required()])
+    my_category = SelectField('Category', choices=[('PickupLine','PickupLine')])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,12 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from .forms import ReviewForm , PitchForm
+from .forms import ReviewForm , PitchFormP, CommentForm
 from ..models import User, Pitch, Review,Comment, UpVote, DownVote
 from flask_login import login_required, current_user
 
 # Views
 @main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
@@ -43,7 +42,7 @@ def Pickupline():
 @login_required
 def Promotion():
 
-    pitch_form = PitchForm()
+    pitch_form = PitchFormP()
 
     if pitch_form.validate_on_submit():
         pitch = pitch_form.pitch.data
@@ -59,3 +58,30 @@ def Promotion():
     title = 'Home | One Minute Pitch'
 
     return render_template("Promotion.html", pitch_form = pitch_form, pitches = all_pitches)
+
+@main.route('/pitch/<int:id>',methods = ['GET','POST'])
+@login_required
+def pitch(id):
+
+    my_pitch = Pitch.query.get(id)
+    comment_form = CommentForm()
+
+    if id is None:
+        abort(404)
+
+    if comment_form.validate_on_submit():
+        comment_data = comment_form.comment.data
+        new_comment = Comment(comment_content = comment_data, pitch_id = id, user = current_user)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.pitch',id=id))
+
+    all_comments = Comment.get_comments(id)
+    # print(all_comments)
+    # format_comments = markdown2.markdown(all_comments.comment_content,extras=["code-friendly", "fenced-code-blocks"])
+
+    up_likes = UpVote.get_votes(id)
+    down_likes = DownVote.get_downvotes(id)
+
+    title = 'Comment | One Minute Pitch'
+    return render_template('pitch.html',pitch = my_pitch, comment_form = comment_form, comments = all_comments, title = title, likes = up_likes, dislikes=down_likes)
