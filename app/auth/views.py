c3aa118a05777b6a892cdc32322024c6d486080c
Francis Karagu <fkaragu@gmail.com>
2019-02-12 15:37:42 +0300

    Added deployment 2

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template,redirect,url_for, flash,request
 from  . import auth
 from flask_login import login_user,logout_user,login_required, current_user
 from ..models import User
-from ..email import send_email, send_reset_email
+
 from .forms import LoginForm,RegistrationForm
 import os
 
